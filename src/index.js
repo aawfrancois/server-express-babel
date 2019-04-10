9824a0e85ebfe2c49fa318e34977298d9c141ca2
aawfrancois <aawfrancois@gmail.com>
2019-04-10 22:46:40 +0200

    add route 404 and file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 "use strict";
 require("dotenv/config");
 import express from 'express'
-
+import path from 'path'
 
 const app = express()
 
@@ -10,4 +10,14 @@ app.get('/', function (req, res) {
     res.send('hello world')
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+//404 for routes undefined
+app.use((req, res, next) => {
+    res.status(404).send('We think you are lost!')
+})
+
+// response with file
+app.get('/docs', function (req, res) {
+    res.sendFile(path.join(__dirname, '../docs/setup_server_node.html'));
+})
+
+app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`))
